08f40beabf7867bb3e561b802c40e9251eef1b3a
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-02-16 00:32:16 +0545

    command code refactor

--- blog/commands.py
+++ blog/commands.py
@@ -1,25 +1,25 @@
+from pydantic import BaseModel, EmailStr
+
 from blog.models import Article, NotFound
-from pydantic import BaseModel, EmailStr 
+
 
 class AlreadyExists(Exception):
-    pass 
+    pass
 
 
 class CreateArticleCommand(BaseModel):
     auther: EmailStr
     title: str
-    content: str  
+    content: str
 
-    def execute(self) -> Article: 
+    def execute(self) -> Article:
         try:
             Article.get_by_title(self.title)
             raise AlreadyExists
         except NotFound:
-            pass 
+            pass
 
         article = Article(auther=self.auther, title=self.title, content=self.content)
-        article.save() 
+        article.save()
 
         return article
-    
-    
