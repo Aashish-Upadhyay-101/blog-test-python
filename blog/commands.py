bf36dc9288598841dd0aa81f1c9434a6dfc11db5
Aashish-Upadhyay-101 <boysmall300@gmail.com>
2023-02-16 00:30:54 +0545

    create article command

--- blog/commands.py
+++ blog/commands.py
@@ -1,4 +1,25 @@
-from blog.models import Article
+from blog.models import Article, NotFound
+from pydantic import BaseModel, EmailStr 
 
 class AlreadyExists(Exception):
     pass 
+
+
+class CreateArticleCommand(BaseModel):
+    auther: EmailStr
+    title: str
+    content: str  
+
+    def execute(self) -> Article: 
+        try:
+            Article.get_by_title(self.title)
+            raise AlreadyExists
+        except NotFound:
+            pass 
+
+        article = Article(auther=self.auther, title=self.title, content=self.content)
+        article.save() 
+
+        return article
+    
+    
